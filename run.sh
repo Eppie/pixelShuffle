7baa144f4ebf545e1b4255592479108b04733fa6
Andrew Epstein <aepstein@sevone.com>
2015-10-04 23:31:46 -0400

    Added runner script.

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+time ./pngRGB images/$1.png images/$2.png results/$1TO$2.png
+time convert -delay 100 images/$1.png -delay 4 out* -delay 200 results/$1TO$2.png -delay 100 images/$2.png -loop 0 results/$1TO$2.gif
+xdg-open results/$1TO$2.gif
+rm out*
