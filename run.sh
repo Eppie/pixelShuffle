5fd7b6d785bf83c554347d2a806f2d636a47c38b
Andrew Epstein <aepstein@sevone.com>
2015-10-10 23:27:33 -0400

    New, bigger files, including some results.

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 time ./pngRGB images/$1.png images/$2.png results/$1TO$2.png
 time convert -delay 100 first.png -delay 4 out* -delay 200 results/$1TO$2.png -delay 100 images/$2.png -loop 0 results/$1TO$2.gif
 xdg-open results/$1TO$2.gif
-rm out* first.png
+rm -f out* first.png
