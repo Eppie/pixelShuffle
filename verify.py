2f145f422e4460e32a3bb60a175850b4c80a0a7e
Andrew Epstein <aepstein@sevone.com>
2016-03-01 14:24:56 -0500

    Added verify script from original stackoverflow post.

---
+++ verify.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+from PIL import Image
+
+
+def check(palette, copy):
+    palette = sorted(Image.open(palette).convert('RGB').getdata())
+    copy = sorted(Image.open(copy).convert('RGB').getdata())
+    print 'Success' if copy == palette else 'Failed'
+
+check('palette.png', 'copy.png')
+
